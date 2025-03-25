22f124160d07937e79f2e118771bce4b5184eb24
Parker2107 <113683584+Parker2107@users.noreply.github.com>
2025-03-25 19:23:56 +0530

    Fixing endpoints

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.indexAll, name="indexAll"),
-    path("check/<str:user_id>", views.check, name="check"),
-    path("delete/<str:user_id>",views.deleteID, name="delete"),
-    path("delete",views.delete, name="delete"),
+    path("check/<str:user_id>/", views.check, name="check"),
+    path("delete/<str:user_id>/",views.deleteID, name="delete"),
+    path("delete/",views.delete, name="delete"),
     path("edit/<str:user_id>", views.edit, name="edit"),
-    path("upload-form", views.formUpload, name="form"),
-    path("get-form/<str:name>",views.getNSForm, name="getNSForm"),
+    path("upload-form/", views.formUpload, name="form"),
+    path("get-form/<str:name>/",views.getNSForm, name="getNSForm"),
     path("keep-alive/", views.keep_alive, name="keep_alive")
 ]
